8628030b7bf7738d98ac98072afa4abba9876733
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-01-19 15:43:51 -0400

    FEAT: Test insert/remove at

--- main.js
+++ main.js
@@ -12,6 +12,43 @@ function main() {
   list.append("turtle");
 
   console.log(list.toString());
+
+  console.log(`Size: ${list.size()}`);
+  console.log('Tail:');
+  console.log(list.tail());
+  console.log(`At 0: ${list.at(0).value}`);
+  console.log(`At 3: ${list.at(3).value}`);
+
+  console.log('Pop');
+  list.pop();
+  console.log(list.toString());
+
+  console.log('Insert tiger at 3. Previously: hamster');
+  list.insertAt('tiger', 3)
+  console.log(list.toString());
+
+  console.log('Remove tiger at 3');
+  list.removeAt(3);
+  console.log(list.toString());
+
+  list.pop();
+  console.log(list.toString());
+  list.pop();
+  console.log(list.toString());
+  list.pop();
+  console.log(list.toString());
+
+  console.log('Contains: dog');
+  console.log(list.contains('dog'));
+  console.log(list.find('dog'));
+
+  list.pop();
+  console.log(list.toString());
+
+  console.log('Contains: dog');
+  console.log(list.contains('dog'));
+  console.log(list.find('dog'));
+
 }
 
 // ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
